cf30d0040e91647c8c905560faf1b4ef417e01ed
roshanyacham <roshanyacham301@gmail.com>
2024-08-28 22:47:18 +0530

    a

--- Day10/Vec_class_Group_class.js
+++ Day10/Vec_class_Group_class.js
@@ -1,21 +1,21 @@
 class Vec {
     constructor(x, y) {
-      this.x = x;
-      this.y = y;
+      this.x = x
+      this.y = y
     }
   
     plus(otherVec) {
-      return new Vec(this.x + otherVec.x, this.y + otherVec.y);
+      return new Vec(this.x + otherVec.x, this.y + otherVec.y)
     }
   
     minus(otherVec) {
-      return new Vec(this.x - otherVec.x, this.y - otherVec.y);
+      return new Vec(this.x - otherVec.x, this.y - otherVec.y)
     }
   
     get length() {
-      return Math.sqrt(this.x ** 2 + this.y ** 2);
+      return Math.sqrt(this.x ** 2 + this.y ** 2)
     }
   }
-console.log(new Vec(1, 2).plus(new Vec(2, 3)));
-console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-console.log(new Vec(3, 4).length);
+console.log(new Vec(1, 2).plus(new Vec(2, 3)))
+console.log(new Vec(1, 2).minus(new Vec(2, 3)))
+console.log(new Vec(3, 4).length)
