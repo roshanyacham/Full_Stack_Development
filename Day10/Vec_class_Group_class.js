da5e8de5740ee02d1fa927f910ede1987908c75a
roshanyacham <roshanyacham301@gmail.com>
2024-08-28 22:49:56 +0530

    d

--- Day10/Vec_class_Group_class.js
+++ Day10/Vec_class_Group_class.js
@@ -19,3 +19,40 @@ class Vec {
 console.log(new Vec(1, 2).plus(new Vec(2, 3)))
 console.log(new Vec(1, 2).minus(new Vec(2, 3)))
 console.log(new Vec(3, 4).length)
+
+
+
+class Group {
+    constructor() {
+      this.members = []
+    }
+  
+    add(value) {
+      if (!this.has(value)) {
+        this.members.push(value)
+      }
+    }
+  
+    delete(value) {
+      this.members = this.members.filter(member => member !== value)
+    }
+  
+    has(value) {
+      return this.members.includes(value)
+    }
+  
+    static from(iterable) {
+      let group = new Group()
+      for (let value of iterable) {
+        group.add(value)
+      }
+      return group
+    }
+  }
+let group = Group.from([10, 20])
+console.log(group.has(10))
+console.log(group.has(30))
+group.add(10);
+group.delete(10);
+console.log(group.has(10))
+  
