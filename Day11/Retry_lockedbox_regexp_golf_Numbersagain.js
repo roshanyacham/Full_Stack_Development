41c8f6b7ea433ac34782f51dacf873ad80d57e56
roshanyacham <roshanyacham301@gmail.com>
2024-08-31 11:12:26 +0530

    The_Locked_Box

--- Day11/Retry_lockedbox_regexp_golf_Numbersagain.js
+++ Day11/Retry_lockedbox_regexp_golf_Numbersagain.js
@@ -1,3 +1,4 @@
+//RETRY
 function primitiveMultiply(a,b){
     if( Math.random()<2.0){
         return a*b
@@ -19,3 +20,46 @@ function reliableMultiply(a,b){
 
 }
 console.log(reliableMultiply(8,8))
+
+//THE LOCKED BOX
+
+const box = new class {
+    locked = true;
+    #content = [];
+    unlock() { this.locked = false; }
+    
+    lock() { this.locked = true; }
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this.#content;
+    }
+    };
+    function withBoxUnlocked(body) {
+        const wasLocked = box.locked
+        if(wasLocked){
+            box.unlock()
+        }
+        try{
+            body()
+        }finally{
+            if(wasLocked){
+                box.lock()
+            }
+        }
+    
+    }
+    withBoxUnlocked(() => {
+        box.content.push("gold piece");
+    });
+    try {
+        withBoxUnlocked(() => {
+            throw new Error("Pirates on the horizon! Abort!");
+    });
+    } catch (e) {
+        console.log("Error raised: " + e);
+    }
+console.log(box.locked);
+
+
+//REGEXP GOLF
+
